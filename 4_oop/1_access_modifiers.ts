53cd4f665d09a839805cd5965d5b1da77530b2d6
Valikhan Akhmedov <>
2020-03-05 00:52:03 +0600

    oop section

--- 4_oop/1_access_modifiers.ts
+++ 4_oop/1_access_modifiers.ts
@@ -1,29 +1,20 @@
 abstract class Model {
-  static a : string;
   constructor(public readonly id: string) {
 
   }
 
-
-  protected convertFromString (): this {
-
-    return new this.constructor('d');
-  }
   public abstract getDisplayName(): string;
 }
 
-type s = ThisType({});
-
-type tt = Model | HTMLAllCollection;
 
 
-class MyClass {
-  public readonly id: string;
-  private readonly _id: string;
-
-
-  public ss() {
-    this.__proto__
+class User extends Model {
+  constructor(
+    public username: string, id: string
+  ) {
+    super(id);
+  }
+  public getDisplayName() {
+    return this.username
   }
 }
-({}).toString();
