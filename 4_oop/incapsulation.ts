851f800ec4c4c8777817f51f229c5e6b5cb44545
Valikhan Akhmedov <>
2020-03-04 21:04:22 +0600

    add incapsulation example

---
+++ 4_oop/incapsulation.ts
@@ -0,0 +1,56 @@
+/**
+ * Bad
+ */
+
+ type CartItem = {}; 
+
+declare class Cart {
+  public items: CartItem[];
+  public count: number;
+  public addItem (item: CartItem): void;
+  public removeItem (item: CartItem): void;
+}
+
+const cart = new Cart();
+cart.items = []
+cart.items.push({}, {}, {});
+cart.count++;
+
+/**
+ * Better
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+declare class BetterCart {
+  public readonly items: Iterable<CartItem>;
+  public get count(): number;
+  public addItem (item: CartItem): void;
+  public removeItem (item: CartItem): void;
+}
+
+
+
+const betterCart = new BetterCart();
+betterCart.items = []
+betterCart.items.push({}, {}, {});
+betterCart.count++;
+
+
+
+/**
+ * DO's 
+ * - Use readonly members
+ * - Use getters
+ * - Hide implementation details by exposing narrowest interface
+ *  - In the example above items could be implemented as Set, or any other data stucture other than array
+ * 
+ * 
+ * User will not even need documentation to use your class
+ */
